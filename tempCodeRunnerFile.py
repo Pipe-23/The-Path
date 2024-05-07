74af8dbfaad1cf20267306aed69f86f067792e2b
Pipe <165952578+Pipe-23@users.noreply.github.com>
2024-05-06 23:48:14 -0600

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+def Guess(x):
+#     random_number = random.randint(1,x)
+#     guess = 0
+#     while guess != random_number:
+#         guess= int(input(f'Guess a number between 1 and {x}:  '))
+
+#         if guess < random_number: {
+#             print("Your guess is too low. Try it again")}
+#         elif guess > random_number:{
+#             print("Your guess is too high. Guess again")}
+
+#     print (f"Congrats you made it! You have guessed the number {random_number} correctly! ")
+
+# Guess(20)
