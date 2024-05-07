af9ad17b31c7b0692ab4a30a5becbb43151a435a
Pipe <165952578+Pipe-23@users.noreply.github.com>
2024-05-06 23:47:41 -0600

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,18 @@
+
+
+youtuber = "Mr Beast"
+
+print("Subscribe to " + youtuber)
+print(f"Subscribe to  {youtuber}")
+
+
+adj = input("Which adjective are you going to use?: ")
+verb1 = input("What verb do you want to use?: ")
+verb2 = input("What is the second verb do you want to use: ")
+famous_person = input("Who is the famous person?: ")
+
+
+madlib = f"Computer is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2}. Like you are {famous_person}!" 
+
+print(madlib) 
